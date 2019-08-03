3cec271cb85ca3226a98fa0dc1523547416fb3a4
peng1998 <1431637430@qq.com>
2019-08-03 15:31:34 +0800

    完成楼层侧边栏跳转效果

---
+++ code/js/index-list.js
@@ -0,0 +1,28 @@
+$(function () {
+    class ceartList {
+        constructor(data) {
+            this.data = data;
+        }
+        init() {
+            $(".prod").append(this.creatHTMLList());
+        }
+        creatHTMLList() {
+            let res = this.data.map((ele) => {
+                let html = `
+        <li>
+            <img src="${ele.img}" alt="">
+            <a href="">${ele.txt}</a>
+            <span>${ele.price}</span>
+        </li>
+        `;
+                return html;
+            }).join("");
+            return res;
+        }
+    }
+
+    $.getJSON("./src/index.json", function (res) {
+        let a = new ceartList(res);
+        a.init();
+    });
+});
--- code/js/index.js
+++ code/js/index.js
@@ -1,17 +1,143 @@
 $(function () {
     class YaoFangWang {
-        constructor() {}
+        constructor() {
+
+        }
         sor() {
             $(window).scroll(function () {
                 var s = $(window).scrollTop();
-
                 if (s > 860) {
                     $(".elevator").show();
-                } else if (s < 700) {
+                    $(".li1 a").addClass("active");
+                }
+                if (s < 700) {
                     $(".elevator").hide();
+                }
+                if (s > 1500) {
+                    $(".li1 a").removeClass("active");
+                    $(".li2 a").addClass("active");
+                } else if (s < 1500) {
+                    $(".li2 a").removeClass("active");
+                }
+                if (s > 2000) {
+                    $(".li2 a").removeClass("active");
+                    $(".li3 a").addClass("active");
+                }
+                if (s < 2000) {
+                    $(".li3 a").removeClass("active");
+                }
+                if (s > 2600) {
+                    $(".li3 a").removeClass("active");
+                    $(".li4 a").addClass("active");
+                }
+                if (s < 2600) {
+                    $(".li4 a").removeClass("active");
+                }
+                if (s > 3200) {
+                    $(".li4 a").removeClass("active");
+                }
+
+                if (s > 3200) {
+                    $(".li5 a").addClass("active");
+                }
+                if (s < 3200) {
+                    $(".li5 a").removeClass("active");
+                }
+                if (s > 3700) {
+                    $(".li5 a").removeClass("active");
+                    $(".li6 a").addClass("active");
+                }
+                if (s < 3700) {
+                    $(".li6 a").removeClass("active");
+                }
+                if (s > 4300) {
+                    $(".li6 a").removeClass("active");
+                }
+            });
+
+            this.loucengTab();
+
+        }
+        loucengTab() {
+            $("#zxyp .snavs a").each((index, ele) => {
+                ele.onmouseenter = function () {
+                    $(ele).css({
+                        "background-color": "#0CB95F",
+                        "color": "#fff",
+                    }).parent().siblings().children("a").css({
+                        "background-color": "#fff",
+                        "color": "#000",
+                    });
+                    $("#zxyp .con-right ul").eq(index).css({
+                        "display": "block",
+                    }).siblings().css("display", "none");
+                }
+            });
+
+
+            $("#ylqx .snavs a").each((index, ele) => {
+                ele.onmouseenter = function () {
+                    $(ele).css({
+                        "background-color": "#0CB95F",
+                        "color": "#fff",
+                    }).parent().siblings().children("a").css({
+                        "background-color": "#fff",
+                        "color": "#000",
+                    });
+                    $("#ylqx .con-right ul").eq(index).css({
+                        "display": "block",
+                    }).siblings().css("display", "none");
+                }
+            });
+
+
+            $("#jsyp .snavs a").each((index, ele) => {
+                ele.onmouseenter = function () {
+                    $(ele).css({
+                        "background-color": "#0CB95F",
+                        "color": "#fff",
+                    }).parent().siblings().children("a").css({
+                        "background-color": "#fff",
+                        "color": "#000",
+                    });
+                    $("#jsyp .con-right ul").eq(index).css({
+                        "display": "block",
+                    }).siblings().css("display", "none");
+                }
+            });
+
+
+            $("#zyyp .snavs a").each((index, ele) => {
+                ele.onmouseenter = function () {
+                    $(ele).css({
+                        "background-color": "#0CB95F",
+                        "color": "#fff",
+                    }).parent().siblings().children("a").css({
+                        "background-color": "#fff",
+                        "color": "#000",
+                    });
+                    $("#zyyp .con-right ul").eq(index).css({
+                        "display": "block",
+                    }).siblings().css("display", "none");
+                }
+            });
+
+            $("#mrhf .snavs a").each((index, ele) => {
+                ele.onmouseenter = function () {
+                    $(ele).css({
+                        "background-color": "#0CB95F",
+                        "color": "#fff",
+                    }).parent().siblings().children("a").css({
+                        "background-color": "#fff",
+                        "color": "#000",
+                    });
+                    $("#mrhf .con-right ul").eq(index).css({
+                        "display": "block",
+                    }).siblings().css("display", "none");
                 }
             });
         }
+
     }
     (new YaoFangWang).sor();
 
